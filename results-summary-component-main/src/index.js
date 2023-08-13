724fa878db99a46acd8471f6f023a6b5e731685e
Tomilko Mihail <114944101+MihailTomilko2003@users.noreply.github.com>
2023-08-13 23:35:48 +0300

    Delete results-summary-component-main directory

--- results-summary-component-main/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import * as ReactDOM from "react-dom/client";
-import App from "./app";
-import "./index.css"
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
